018d1f90da4c733b53e0323174168a9ade558ee9
NateQuattrochi4615 <quattrochi.n@northeastern.edu>
2021-04-23 12:06:13 -0400

    data offload task v1

---
+++ H750VBT6-flight-board-v0/System/Tasks/data_offload.h
@@ -0,0 +1,15 @@
+
+#ifndef TASKS_DATA_OFFLOAD_H_
+#define TASKS_DATA_OFFLOAD_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+	bool dataOffload();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TASKS_DATA_OFFLOAD_H_ */
