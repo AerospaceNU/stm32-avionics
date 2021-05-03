dc78146c730df8524397c9b1f3c3d7c6f922ebde
tykosa <tkosakowski@starry.com>
2021-05-03 14:57:43 -0400

    Make functions pure virtual in states_interface

--- System/States/states_interface.h
+++ System/States/states_interface.h
@@ -23,7 +23,7 @@ class State {
 
     public:
 		State(int id, uint32_t period_ms): id_(id), period_ms_(period_ms) { }
-		virtual ~State() = default;
+		virtual ~State() {}
 
 		/**
 		 * @brief Returns given ID of this state so higher level can track State pointers instead of IDs
@@ -35,7 +35,7 @@ class State {
 		 * @brief Gets the current value of the run counter
 		 * @return Run counter value
 		 */
-		uint32_t getRunCounter() { return run_counter_; };
+		uint32_t getRunCounter() { return run_counter_; }
 
 		/**
 		 * @brief Runs this state and increments the counter
@@ -49,12 +49,12 @@ class State {
 		/**
 		 * @brief Actions that occur on initialization of state
 		 */
-        virtual void init(void);
+        virtual void init(void) = 0;
 
         /**
          * @brief Actions that occur at the end of
          */
-        virtual void cleanup(void);
+        virtual void cleanup(void) = 0;
 
         /**
          * @brief Returns rate state should run as a period in MS
@@ -62,6 +62,7 @@ class State {
         virtual uint32_t getPeriodMS(void) { return period_ms_; }
 
     private:
+
         /**
          * @brief Actions that occur on every loop of state
          * @return End condition of the current loop
