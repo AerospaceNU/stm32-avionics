e1359ab264f70ffaca931efeead2930cfb3e0fda
NateQuattrochi4615 <quattrochi.n@northeastern.edu>
2021-05-04 18:48:13 -0400

    removed dataoffload from climain

--- H750VBT6-flight-board-v0/System/States/state_cli_main.cpp
+++ H750VBT6-flight-board-v0/System/States/state_cli_main.cpp
@@ -3,7 +3,6 @@
 #include "state_cli_main.h"
 
 #include "hardware_manager.h"
-#include"data_offload.h"
 
 void CliMainState::init() {
 
@@ -11,7 +10,6 @@ void CliMainState::init() {
 
 EndCondition_t CliMainState::run() {
 	buzzerHeartbeat();
-	dataOffload();
 	// Just check USB for now
 	// TODO: Check for valid configuration after CLI implemented
 	if (!HM_UsbIsConnected()) {
