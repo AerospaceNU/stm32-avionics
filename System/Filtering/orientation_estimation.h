5711c6da3746295a89a88cd6bad8f0cd165cd783
SamHaggans <sam@samhaggans.com>
2022-03-21 18:31:10 -0400

    WIP orientation estimation

---
+++ System/Filtering/orientation_estimation.h
@@ -0,0 +1,15 @@
+/*
+ * orientation_estimation.h
+ *
+ *  Created on: Mar 21, 2022
+ *      Author: sam
+ */
+
+#ifndef ORIENTATION_ESTIMATION_H_
+#define ORIENTATION_ESTIMATION_H_
+
+
+
+
+
+#endif /* ORIENTATION_ESTIMATION_H_ */
