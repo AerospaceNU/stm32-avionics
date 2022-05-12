bad781826001c36332d07ad64a7cf7159eb0d606
SamHaggans <sam@samhaggans.com>
2022-05-11 22:58:46 -0400

    Add orientation estimation

--- System/Filtering/orientation_estimation.cpp
+++ System/Filtering/orientation_estimation.cpp
@@ -4,12 +4,47 @@
  *  Created on: Mar 21, 2022
  *      Author: sam
  */
-
 #include "orientation_estimation.h"
+
+#include <math.h>
+
 #include "matrix.h"
 
+OrientationEstimation::OrientationEstimation(double dt) : m_dt(dt) {}
 
+void OrientationEstimation::reset() { this->q = Matrix<4, 1>({1.0, 0, 0, 0}); }
 
+void OrientationEstimation::setDt(double dt) { this->m_dt = dt; }
 
+void OrientationEstimation::setAccelVector(float accels[]) {
+  // From
+  // https://github.com/Mayitzin/ahrs/blob/87d27880fd903be3762c68000cc5dd41c720200d/ahrs/common/orientation.py#L923
+  Matrix<3, 1> a({accels[0], accels[1], accels[2]});
+  float a_norm = a.norm();
+  float ax = accels[0] / a_norm;
+  float ay = accels[1] / a_norm;
+  float az = accels[2] / a_norm;
+  float ex = atan2(ay, az);
+  float ey = atan2(-ax, sqrt(pow(ay, 2) + pow(az, 2)));
+  float cx2 = cos(ex / 2.0);
+  float sx2 = sin(ex / 2.0);
+  float cy2 = cos(ey / 2.0);
+  float sy2 = sin(ey / 2.0);
+  this->q = Matrix<4, 1>({cx2 * cy2, sx2 * cy2, cx2 * sy2, -sx2 * sy2});
+  this->q = q * (1.0 / q.norm());
+}
 
+void OrientationEstimation::update(float gyr[]) {
+  // From
+  // https://github.com/Mayitzin/ahrs/blob/87f9210cfcf6c545d86ae8588a93f012020164ee/ahrs/filters/angular.py#L374
+  Matrix<4, 4> omega({0.0, -gyr[0], -gyr[1], -gyr[2], gyr[0], 0.0, gyr[2],
+                      -gyr[1], gyr[1], -gyr[2], 0.0, gyr[0], gyr[2], gyr[1],
+                      -gyr[0], 0.0});
+  Matrix<3, 1> gyro({gyr[0], gyr[1], gyr[2]});
 
+  float w = gyro.norm();
+  Matrix<4, 4> A = (Matrix<4, 4>::identity() * cos(w * m_dt / 2.0)) +
+                   (omega * (1.0 / w) * sin(2 * m_dt / 2.0));
+  this->q = A * q;
+  this->q = q * (1.0 / q.norm());
+}
