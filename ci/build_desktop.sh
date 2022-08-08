d2a91f8f23bab68cbd6750f6e395fd4c09f2cdb5
SamHaggans <sam@samhaggans.com>
2022-08-07 21:59:35 -0400

    Fix git info creation for windows

--- ci/build_desktop.sh
+++ ci/build_desktop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo -e "#ifndef _GIT_INFO_\n#define _GIT_INFO_\n#ifdef __cplusplus\nextern \"C\" {\n#endif" > ./Utils/git_info.h && echo -e "const char* GIT_DESCRIBE = \"`git describe`\";" >> ./Utils/git_info.h && echo -e `git log --pretty=format:"const char* GIT_COMMIT_TIME =\"%ci\";\nconst char* GIT_COMMIT_MESSAGE =\"%<(50,trunc)%s\";" -n 1` >> ./Utils/git_info.h && echo -e "#ifdef __cplusplus\n}\n#endif\n#endif" >> ./Utils/git_info.h
-
+(echo "#ifndef UTILS_GIT_INFO_H_" && echo "#define UTILS_GIT_INFO_H_" && echo "#ifdef __cplusplus" && echo "extern \"C\" {" && echo "#endif" && echo "const char* GIT_DESCRIBE = \"`git describe`\";" && echo `git log --pretty=format:"const char* GIT_COMMIT_TIME = \"%ci\";" -n 1` && echo `git log --pretty=format:"const char* GIT_COMMIT_MESSAGE = \"%<(50,trunc)%s\";" -n 1` && echo "#ifdef __cplusplus" && echo "}" && echo "#endif" && echo "#endif  // UTILS_GIT_INFO_H_") > ./Utils/git_info.h
 mkdir build
 cd build
 cmake ..
