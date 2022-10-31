6d0ddcac63084f1a544e30f73df9103617f5e4aa
Matt M <matthew.morley.ca@gmail.com>
2022-10-31 16:34:45 -0400

    Updates for FAT32

--- flash_expariments/metaprogramming.py
+++ flash_expariments/metaprogramming.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-image = open("flash_expariments/big.img", "rb")
+image = open("big.img", "rb")
 
 def list_to_string(list):
     return ",".join(map(lambda it: str(it), list))
 
 # First, generate first boot sector
-boots = image.read(512)
+boots = image.read(0xe00)
 boots = list(boots)
 print(f"uint8_t boots[] = {'{'}{list_to_string(boots)}{'}'};")
 
