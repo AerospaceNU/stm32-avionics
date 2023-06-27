42ec21d7728c6e11b6285922e264f1f7f2df7771
--global <matthew.morley.ca@gmail.com>
2023-06-26 20:36:04 -0700

    Create common entrypoint

---
+++ common/system/entrypoint.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_SYSTEM_ENTRYPOINT_H_
+#define COMMON_SYSTEM_ENTRYPOINT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void main_cpp();
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* COMMON_SYSTEM_ENTRYPOINT_H_ */
--- stm32_projects/fcb_v1/Core/Src/main.c
+++ stm32_projects/fcb_v1/Core/Src/main.c
@@ -33,7 +33,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "scheduler.h"
+#include "entrypoint.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -58,7 +58,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-void PeriphCommonClock_Config(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -90,9 +89,6 @@ int main(void) {
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* Configure the peripherals common clocks */
-  PeriphCommonClock_Config();
-
   /* USER CODE BEGIN SysInit */
 
   /* USER CODE END SysInit */
@@ -101,27 +97,23 @@ int main(void) {
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
-  MX_ADC2_Init();
-  MX_ADC3_Init();
-  MX_I2C1_Init();
-  MX_RTC_Init();
-  MX_SPI1_Init();
   MX_SPI3_Init();
   MX_SPI4_Init();
-  MX_SPI6_Init();
-  MX_TIM1_Init();
   MX_TIM8_Init();
-  MX_UART5_Init();
+  MX_SPI2_Init();
+  MX_I2C2_Init();
+  MX_SPI1_Init();
+  MX_TIM1_Init();
   MX_USB_DEVICE_Init();
+  MX_RTC_Init();
+  MX_UART4_Init();
+  MX_UART7_Init();
   MX_IWDG1_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
-  /* Initialize scheduler */
-  Scheduler scheduler = Scheduler();
+  main_cpp();
 
-  /* Scheduler will never return. NO CODE SHOULD COME AFTER THIS IN THIS FILE */
-  scheduler.run();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -170,12 +162,12 @@ void SystemClock_Config(void) {
   RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 5;
-  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 60;
   RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 15;
+  RCC_OscInitStruct.PLL.PLLQ = 5;
   RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
   RCC_OscInitStruct.PLL.PLLFRACN = 0;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
@@ -197,30 +189,9 @@ void SystemClock_Config(void) {
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
     Error_Handler();
   }
-}
-
-/**
- * @brief Peripherals Common Clock Configuration
- * @retval None
- */
-void PeriphCommonClock_Config(void) {
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-
-  /** Initializes the peripherals clock
+  /** Enables the Clock Security System
    */
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-  PeriphClkInitStruct.PLL2.PLL2M = 2;
-  PeriphClkInitStruct.PLL2.PLL2N = 12;
-  PeriphClkInitStruct.PLL2.PLL2P = 2;
-  PeriphClkInitStruct.PLL2.PLL2Q = 2;
-  PeriphClkInitStruct.PLL2.PLL2R = 2;
-  PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_3;
-  PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOMEDIUM;
-  PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
-  PeriphClkInitStruct.AdcClockSelection = RCC_ADCCLKSOURCE_PLL2;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
-    Error_Handler();
-  }
+  HAL_RCC_EnableCSS();
 }
 
 /* USER CODE BEGIN 4 */
