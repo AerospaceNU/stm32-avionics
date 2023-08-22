895d04606da4ddad710f64e4f4216f068158377b
Matt <matthew.morley.ca@gmail.com>
2023-08-21 23:42:48 -0700

    serialib spam repeats last stuff??

--- libusb_test/main.cpp
+++ libusb_test/main.cpp
@@ -1,3 +1,4 @@
+#if 0
 /**
  * @file /example1/main.cpp
  * @author Philippe Lucidarme
@@ -29,7 +30,6 @@
 
 // Serial library
 #include "serialib.h"
-#include <unistd.h>
 #include <stdio.h>
 
 
@@ -37,7 +37,7 @@
     //for serial ports above "COM9", we must use this extended syntax of "\\.\COMx".
     //also works for COM0 to COM9.
     //https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea?redirectedfrom=MSDN#communications-resources
-    #define SERIAL_PORT "\\\\.\\COM1"
+    #define SERIAL_PORT "\\\\.\\COM14"
 #endif
 #if defined (__linux__) || defined(__APPLE__)
     #define SERIAL_PORT "/dev/ttyACM0"
@@ -67,64 +67,69 @@ int main( /*int argc, char *argv[]*/)
     // printf ("String sent: %s", buffer);
 
     // Read the string
-    serial.readString(buffer, '\n', 200, 2000);
-    printf("String read: %s\n", buffer);
+    while (true) {
+        serial.readString(buffer, '\n', 200, 2000);
+        printf("String read: %s\n", buffer);
+    }
 
     // Close the serial device
     serial.closeDevice();
 
     return 0 ;
 }
+#endif
 
+#if 1
 
-// // File: test.cpp
-// // Description: Serial (Com port) console program for Windows and Linux
-// // WebSite: http://cool-emerald.blogspot.sg/2017/05/serial-port-programming-in-c-with.html
-// // MIT License (https://opensource.org/licenses/MIT)
-// // Copyright (c) 2018 Yan Naing Aye
+// File: test.cpp
+// Description: Serial (Com port) console program for Windows and Linux
+// WebSite: http://cool-emerald.blogspot.sg/2017/05/serial-port-programming-in-c-with.html
+// MIT License (https://opensource.org/licenses/MIT)
+// Copyright (c) 2018 Yan Naing Aye
 
-// #include <stdio.h>
-// #define CE_SERIAL_IMPLEMENTATION
-// #include "ceSerial.h"
-// using namespace std;
+#include <stdio.h>
+#define CE_SERIAL_IMPLEMENTATION
+#include "ceSerial.h"
+using namespace std;
 
-// int main()
-// {
-// #ifdef CE_WINDOWS
-// 	ceSerial com("\\\\.\\COM14",9600,8,'N',1); // Windows
-// #else
-// 	ceSerial com("/dev/ttyACM0",9600,8,'N',1); // Linux
-// #endif
+int main()
+{
+#ifdef CE_WINDOWS
+	ceSerial com("\\\\.\\COM14",9600,8,'N',1); // Windows
+#else
+	ceSerial com("/dev/ttyACM0",9600,8,'N',1); // Linux
+#endif
 
-// 	printf("Opening port %s.\n",com.GetPort().c_str());
-// 	if (com.Open() == 0) {
-// 		printf("OK.\n");
-// 	}
-// 	else {
-// 		printf("Error.\n");
-// 		return 1;
-// 	}
+	printf("Opening port %s.\n",com.GetPort().c_str());
+	if (com.Open() == 0) {
+		printf("OK.\n");
+	}
+	else {
+		printf("Error.\n");
+		return 1;
+	}
 
-// 	bool successFlag;
-// 	// printf("Writing.\n");
-// 	// char s[]="Hello";
-// 	// successFlag=com.Write(s); // write string
-// 	// successFlag=com.WriteChar('!'); // write a character
+	bool successFlag;
+	// printf("Writing.\n");
+	// char s[]="Hello";
+	// successFlag=com.Write(s); // write string
+	// successFlag=com.WriteChar('!'); // write a character
 
-// 	// printf("Waiting 3 seconds.\n");
-// 	// ceSerial::Delay(3000); // delay to wait for a character
+	// printf("Waiting 3 seconds.\n");
+	// ceSerial::Delay(3000); // delay to wait for a character
 
-// 	printf("Reading.\n");
-// 	for (int i = 0; i < 1000; i++) {
-// 		ceSerial::Delay(5); // delay to wait for a character
-//         char outArr[2000] = {0};
-//         com.Read(outArr, sizeof(outArr) - 1);
-//         printf(outArr); // assume null-terminated
-//         // char c=com.ReadChar(successFlag); // read a char
-//         // if(successFlag) printf("%c",c);
-//     }
+	printf("Reading.\n");
+	for (int i = 0; i < 1000; i++) {
+		ceSerial::Delay(5); // delay to wait for a character
+        char outArr[2000] = {0};
+        com.Read(outArr, sizeof(outArr) - 1);
+        printf(outArr); // assume null-terminated
+        // char c=com.ReadChar(successFlag); // read a char
+        // if(successFlag) printf("%c",c);
+    }
 
-// 	printf("Closing port %s.\n",com.GetPort().c_str());
-// 	com.Close();
-// 	return 0;
-// }
+	printf("Closing port %s.\n",com.GetPort().c_str());
+	com.Close();
+	return 0;
+}
+#endif
