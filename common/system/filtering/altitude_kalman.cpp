ea771cd763463799e4409b062d0f89cfe963ed05
mpimble <pimblemichael@yahoo.com>
2023-11-20 22:12:41 -0500

    Fixed dt check

--- common/system/filtering/altitude_kalman.cpp
+++ common/system/filtering/altitude_kalman.cpp
@@ -36,9 +36,12 @@ const AltitudeKalmanOutput_s AltitudeKalman::getXhat() const { return xHat; }
 void AltitudeKalman::setDt(double dt) { m_dt = dt; }
 
 void AltitudeKalman::calculateDt() {
-  m_dt = ((current_ts == 0) || !has_ran || (m_dt == 0))
+  m_dt = ((current_ts == 0) || !has_ran)
              ? 0.015
              : (double)(current_ts - last_ts) / 1000.0;
+  if (m_dt == 0) {
+    m_dt = 0.015;
+  }
   has_ran = true;
 }
 
