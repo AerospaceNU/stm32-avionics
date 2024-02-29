7bf58413334974b8fade01e135ac15f35fc7bf5f
Matt <matthew.morley.ca@gmail.com>
2024-02-28 19:48:28 -0500

    reeeeeeeee

--- platformio_projects/src/mini_groundstation.cpp
+++ platformio_projects/src/mini_groundstation.cpp
@@ -17,35 +17,37 @@
 #define SX1262_CS 6
 #define SX1262_INT 7
 
+#define RAD_MISO 3
+#define RAD_MOSI 4
+#define RAD_SCK 8
+
 // FCB Code
 // Groundstation groundstation;
 
 // Drivers
 // Sx1262 sx1262;
 
-static SPIClass spi(HSPI);
+// static SPIClass spi(HSPI);
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Hello!");
 
-  spi.begin(8, 3, 4, SX1262_CS);
-
-  //  Serial.begin(115200);
-
-  // Init drivers
-  //  sx1262.init(SX1262_RST, SX1262_CS, SX1262_INT);
-
-  // Add them to device manager
-  //  auto deviceManager = getDeviceManager();
-  //  deviceManager->addRadio(&sx1262);
-
-  // Run FCB code
-  //  groundstation.init();
 
   pinMode(SX1262_RST, OUTPUT);
   pinMode(SX1262_CS, OUTPUT);
+  pinMode(RAD_MISO, OUTPUT);
+  pinMode(RAD_MOSI, OUTPUT);
+  pinMode(RAD_SCK, OUTPUT);
+
+  Serial.setTimeout(30000);
+  Serial.print("Waiting");
+  Serial.readStringUntil('\n');
+  Serial.print("Making CS high");
 
   digitalWrite(SX1262_CS, HIGH);
+
+  // spi.begin(RAD_SCK, RAD_MISO, RAD_MOSI, SX1262_CS);
 
   delay(100);
   digitalWrite(SX1262_RST, LOW);
@@ -54,34 +56,54 @@ void setup() {
   delay(100);
 }
 
-SPISettings spiSettings(100000, MSBFIRST, SPI_MODE0);
+SPISettings spiSettings(10000, MSBFIRST, SPI_MODE0);
 
 void loop() {
-  //  groundstation.runOnce();
+  Serial.println("Waiting in loop");
+  Serial.readStringUntil('\n');
 
-  int size = 3;
+  int size = 2;
   uint8_t data[size];
   memset(data, 0, size);
   data[0] = 0xC0;
-
-  spi.beginTransaction(spiSettings);
+  Serial.println("Starting data transfer");
+  // spi.beginTransaction(spiSettings);
 
   digitalWrite(SX1262_CS, LOW);
-  Serial.print(spi.transfer(0xC0));
-  Serial.print(" ");
-  Serial.print(spi.transfer(0));
-  Serial.print(" ");
-  Serial.print(spi.transfer(0));
-  Serial.println();
+  digitalWrite(RAD_SCK, LOW);
+  delayMicroseconds(1);
+  uint8_t dataOut[3] = {0};
+  for (int i = 0; i < 2; i++) {
+    for (int j = 7; j >= 0; j--) {
+      auto bit = (data[i] >> j) & 1;
+      digitalWrite(RAD_MOSI, bit);
+
+      digitalWrite(RAD_SCK, HIGH);
+      delayMicroseconds(1);
+      dataOut[i] |= (digitalRead(RAD_MISO) << j);
+      delayMicroseconds(1);
+      digitalWrite(RAD_SCK, LOW);
+    }
+  }
+    
   digitalWrite(SX1262_CS, HIGH);
 
-  spi.endTransaction();
 
-  //  Serial.print(data[0]);
-  //  Serial.print(" ");
-  //  Serial.print(data[1]);
-  //  Serial.print(" ");
-  //  Serial.println(data[2]);
+  for (int i = 0; i < 3; i++) {
+    Serial.print("Got: "); Serial.println(dataOut[i]);
+  }
 
-  delay(1000);
+  // digitalWrite(SX1262_CS, LOW);
+  // Serial.print(spi.transfer(0xC0));
+  // Serial.print(" ");
+  // Serial.print(spi.transfer(0));
+  // Serial.print(" ");
+  // Serial.print(spi.transfer(0));
+  // Serial.println();
+  // digitalWrite(SX1262_CS, HIGH);
+
+  // spi.endTransaction();
+
+
+  delay(2000);
 }
