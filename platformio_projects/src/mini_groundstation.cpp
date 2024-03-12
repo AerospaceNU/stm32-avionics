3f0e6f5a442097f16300ca66335358f622882ebe
Matt <matthew.morley.ca@gmail.com>
2024-03-12 16:23:08 -0400

    asdf

--- platformio_projects/src/mini_groundstation.cpp
+++ platformio_projects/src/mini_groundstation.cpp
@@ -16,7 +16,9 @@
 #define SX1262_RST 2
 #define SX1262_CS 6
 #define SX1262_INT 7
-
+#define SX1262_BUSY 5
+#define SX1262_DIO1 7
+#define SX1262_DIO3 10
 #define RAD_MISO 3
 #define RAD_MOSI 4
 #define RAD_SCK 8
@@ -35,9 +37,10 @@ void setup() {
 
   pinMode(SX1262_RST, OUTPUT);
   pinMode(SX1262_CS, OUTPUT);
-  // pinMode(RAD_MISO, OUTPUT);
-  // pinMode(RAD_MOSI, OUTPUT);
-  // pinMode(RAD_SCK, OUTPUT);
+  pinMode(SX1262_BUSY, INPUT);
+  pinMode(SX1262_DIO1, INPUT);
+  pinMode(SX1262_DIO3, INPUT);
+
 
   Serial.setTimeout(30000);
   Serial.print("Waiting");
@@ -59,19 +62,39 @@ void setup() {
 #define spi SPI
 SPISettings spiSettings(1000000, MSBFIRST, SPI_MODE0);
 
+void RadioSpiTransfer(uint8_t *arr, size_t len) {
+  Serial.println("Output arr:");
+  for (int i = 0; i < len; i++) {
+    Serial.print(arr[i]); Serial.print(" ");
+  }
+  Serial.println();
+ 
+  digitalWrite(SX1262_CS, LOW);
+  SPI.beginTransaction(spiSettings);
+  for (auto i = 0; i < len; i++) {
+    arr[i] = SPI.transfer(arr[i]);
+  }
+  SPI.endTransaction();
+  digitalWrite(SX1262_CS, HIGH);
+
+  Serial.println("Got back:");
+  for (int i = 0; i < len; i++) {
+    Serial.print(arr[i]); Serial.print(" ");
+  }
+  Serial.println();
+}
+
 void loop() {
   Serial.println("Waiting in loop");
   Serial.readStringUntil('\n');
 
-  digitalWrite(SX1262_CS, LOW);
-  SPI.beginTransaction(spiSettings);
-  Serial.print(spi.transfer(0xC0));
-  Serial.print(" ");
-  Serial.print(spi.transfer(0), BIN);
-  Serial.println();
-  SPI.endTransaction();
-  digitalWrite(SX1262_CS, HIGH);
+  uint8_t arr[] = {0xc0, 0};
+  RadioSpiTransfer(arr, sizeof(arr));
 
+  // wait 1uS then check busy
+  delayMicroseconds(1);
+  while (digitalRead(SX1262_BUSY)) delayMicroseconds(1);
+  Serial.println("Busy low again!");
 
   delay(2000);
 }
