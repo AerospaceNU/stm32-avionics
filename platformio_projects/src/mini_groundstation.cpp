3791234c43ad89e0b04c8370d7acdccccfcdf589
Nathan McGuire <nathan.e.mcguire@gmail.com>
2024-03-18 22:29:45 -0400

    Made a bunch of tweaks, but probably didn't get anywhere

--- platformio_projects/src/mini_groundstation.cpp
+++ platformio_projects/src/mini_groundstation.cpp
@@ -7,10 +7,11 @@
 // https://github.com/espressif/esptool/issues/836
 
 #include <Arduino.h>
+#include <SPI.h>
 #include <arduino_hardware_manager.h>
 #include <groundstation.h>
+
 #include <radio/custom_SX126x.hpp>
-#include <SPI.h>
 
 // Pin definitions
 // Radio
@@ -25,28 +26,44 @@
 #define RAD_MOSI 4
 #define RAD_SCK 8
 
-SPISettings spiSettings(1000000, MSBFIRST, SPI_MODE0);
+SPISettings spiSettings(1000000, SPI_MSBFIRST, SPI_MODE0);
 
 class ArduinoSx126x : public SX126x {
-	uint8_t HalGpioRead(GpioPinFunction_t func) {
+  uint8_t HalGpioRead(GpioPinFunction_t func) {
     switch (func) {
-      case GPIO_PIN_RESET: return digitalRead(SX1262_RST);
-      case GPIO_PIN_BUSY: return digitalRead(SX1262_BUSY);
-      case GPIO_PIN_DIO1: return digitalRead(SX1262_DIO1);
-      case GPIO_PIN_DIO3: return digitalRead(SX1262_DIO1);
-      default: return 0;
+      case GPIO_PIN_RESET:
+        return digitalRead(SX1262_RST);
+      case GPIO_PIN_BUSY:
+        return digitalRead(SX1262_BUSY);
+      case GPIO_PIN_DIO1:
+        return digitalRead(SX1262_DIO1);
+      case GPIO_PIN_DIO3:
+        return digitalRead(SX1262_DIO1);
+      default:
+        return 0;
     }
   }
-	void HalGpioWrite(GpioPinFunction_t func, uint8_t value) {
+
+  void HalGpioWrite(GpioPinFunction_t func, uint8_t value) {
     switch (func) {
-      case GPIO_PIN_RESET: digitalWrite(SX1262_RST, value); break;
-      case GPIO_PIN_BUSY: digitalWrite(SX1262_BUSY, value); break;
-      case GPIO_PIN_DIO1: digitalWrite(SX1262_DIO1, value); break;
-      case GPIO_PIN_DIO3: digitalWrite(SX1262_DIO1, value); break;
-      default: break;
+      case GPIO_PIN_RESET:
+        digitalWrite(SX1262_RST, value);
+        break;
+      case GPIO_PIN_BUSY:
+        digitalWrite(SX1262_BUSY, value);
+        break;
+      case GPIO_PIN_DIO1:
+        digitalWrite(SX1262_DIO1, value);
+        break;
+      case GPIO_PIN_DIO3:
+        digitalWrite(SX1262_DIO1, value);
+        break;
+      default:
+        break;
     }
   }
-	void HalSpiTransfer(uint8_t *buffer_in, const uint8_t *buffer_out, uint16_t size) {
+
+  void HalSpiTransfer(uint8_t *buffer_in, const uint8_t *buffer_out, uint16_t size) {
     digitalWrite(SX1262_CS, LOW);
     SPI.beginTransaction(spiSettings);
     SPI.transferBytes(buffer_out, buffer_in, size);
@@ -66,6 +83,19 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Hello!");
 
+  //Start with chip select high
+  pinMode(SX1262_CS, OUTPUT);
+  digitalWrite(SX1262_CS, HIGH);
+  delay(5);
+
+  //Toggle reset pin
+  pinMode(SX1262_RST, OUTPUT);
+  digitalWrite(SX1262_RST, HIGH);
+  delay(10);
+  digitalWrite(SX1262_RST, LOW);
+
+  pinMode(SX1262_INT, INPUT);
+
   Serial.setTimeout(90000);
   Serial.println("Waiting");
   Serial.readStringUntil('\n');
@@ -80,21 +110,21 @@ void loop() {
   Serial.println("Waiting in loop");
   Serial.readStringUntil('\n');
 
-	SX126x::ModulationParams_t ModulationParams2;
-	ModulationParams2.PacketType = SX126x::PACKET_TYPE_LORA;
-	ModulationParams2.Params.LoRa.CodingRate = SX126x::LORA_CR_4_6;
-	ModulationParams2.Params.LoRa.Bandwidth = SX126x::LORA_BW_500;
-	ModulationParams2.Params.LoRa.SpreadingFactor = SX126x::LORA_SF10;
-	ModulationParams2.Params.LoRa.LowDatarateOptimize = 0;
+  SX126x::ModulationParams_t ModulationParams2;
+  ModulationParams2.PacketType = SX126x::PACKET_TYPE_LORA;
+  ModulationParams2.Params.LoRa.CodingRate = SX126x::LORA_CR_4_6;
+  ModulationParams2.Params.LoRa.Bandwidth = SX126x::LORA_BW_500;
+  ModulationParams2.Params.LoRa.SpreadingFactor = SX126x::LORA_SF10;
+  ModulationParams2.Params.LoRa.LowDatarateOptimize = 0;
 
-	SX126x::PacketParams_t PacketParams2;
-	PacketParams2.PacketType = SX126x::PACKET_TYPE_LORA;
-	auto &l = PacketParams2.Params.LoRa;
-	l.PayloadLength = 253;
-	l.HeaderType = SX126x::LORA_PACKET_FIXED_LENGTH;
-	l.PreambleLength = 12;
-	l.CrcMode = SX126x::LORA_CRC_ON;
-	l.InvertIQ = SX126x::LORA_IQ_NORMAL;
+  SX126x::PacketParams_t PacketParams2;
+  PacketParams2.PacketType = SX126x::PACKET_TYPE_LORA;
+  auto &l = PacketParams2.Params.LoRa;
+  l.PayloadLength = 253;
+  l.HeaderType = SX126x::LORA_PACKET_FIXED_LENGTH;
+  l.PreambleLength = 12;
+  l.CrcMode = SX126x::LORA_CRC_ON;
+  l.InvertIQ = SX126x::LORA_IQ_NORMAL;
 
   radio.SetModulationParams(ModulationParams2);
   radio.SetPacketParams(PacketParams2);
@@ -103,13 +133,19 @@ void loop() {
   radio.SetDio2AsRfSwitchCtrl(true);
 
   radio.SetBufferBaseAddresses(0, 128);
-  Serial.print("Errors on configuration: "); Serial.println(radio.GetDeviceErrors().Value);
+  Serial.print("Errors on configuration: ");
+  Serial.println(radio.GetDeviceErrors().Value);
 
   uint8_t testPayload[32];
-  for (int i = 0; i < sizeof(testPayload); i++) testPayload[i] = i;
+  for (int i = 0; i < sizeof(testPayload); i++) {
+    testPayload[i] = i;
+  }
   radio.SetPayload(testPayload, sizeof(testPayload));
   radio.SetTx(0);
-  Serial.print("Errors on TX: "); Serial.println(radio.GetDeviceErrors().Value);
+  Serial.print("Errors on TX: ");
+  Serial.println(radio.GetDeviceErrors().Value);
+
+  Serial.println("\n\n\n");
 
   delay(5000);
 }
