cd7bd162e8078d2e69f819da3d3f3ea5a5a88e34
SamHaggans <sam@samhaggans.com>
2024-10-05 16:12:41 -0400

    Additional command queue updates

--- stm32_projects/amazon-F401-breakout/Core/Src/entrypoint.cpp
+++ stm32_projects/amazon-F401-breakout/Core/Src/entrypoint.cpp
@@ -1,18 +1,29 @@
 #include "DynamixelMotor.h"
+#include "dynamixel_command_queue.h"
 #include "stm32f4xx_hal.h"
 
 extern "C" {  // another way
 void entrypoint(UART_HandleTypeDef* huart);
 };
 
+uint32_t lastTimestamp;
+
 void entrypoint(UART_HandleTypeDef* huart) {
-  DynamixelMotor m{huart};
-  m.torqueEnable(Toggle::ON);
-  HAL_Delay(1000);
+  DynamixelCommandQueue commandQueue{huart};
+  //DynamixelMotor m1{1, &commandQueue};
+  DynamixelMotor m2{2, &commandQueue};
+  //m1.setOperatingMode(OperatingMode::EXT_POSITION);
+  m2.setOperatingMode(OperatingMode::EXT_POSITION);
+  //m1.torqueEnable(Toggle::ON);
+  m2.torqueEnable(Toggle::ON);
   double degrees = 0;
-  while (degrees < 360) {
-    m.goalPosition(degrees);
-    HAL_Delay(5000);
+  while (1) {
+    //m1.goalPosition(degrees);
+    m2.goalPosition(degrees);
+    lastTimestamp = HAL_GetTick();
+    while (HAL_GetTick() - lastTimestamp < 5000) {
+    	commandQueue.tick();
+    }
     degrees += 60;
   }
 }
